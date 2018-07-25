e00b26b6b8220e5b1b21a98953df6d61cd53cc2c
Gabriela <g.saavedra.letelier@gmail.com>
2018-07-25 13:29:50 +0200

    add Main class and lanterna

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.terminal.Terminal;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
