2167aeae769edfe2e7cd51163fb4293d3655684d
Gabriela <g.saavedra.letelier@gmail.com>
2018-07-25 12:37:14 +0200

    test

---
+++ src/Playingcard.java
@@ -0,0 +1,12 @@
+public class Playingcard {
+    int valör;
+    String färg;
+
+
+    public Playingcard(int valör, String färg) {
+        this.valör = valör;
+        this.färg = färg;
+    }
+
+
+}
